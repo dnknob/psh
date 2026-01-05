6c2b976f109b69d66c6dd5d9966e1148d5afdd83
0x160000 <openbsd.gnu@gmail.com>
2026-01-05 11:42:32 +0100

    replace hardcoded max arguments with malloc

--- main.c
+++ main.c
@@ -7,7 +7,6 @@
 #include <signal.h>
 #include <unistd.h>
 
-#define MAXARGS 16
 #define MAXJOBS 64
 
 extern int builtin_cd(int argc, char *argv[]);
@@ -59,18 +58,25 @@ int main(void) {
         inb[strcspn(inb, "\n")] = 0;
         if (strlen(inb) == 0) continue;
 
-        char *argv[MAXARGS];
-        int argc = 0;
+        int capacity = strlen(inb) / 2 + 2;
+        char **argv = malloc(capacity * sizeof(char *));
+        if (!argv) {
+            perror("malloc");
+            continue;
+        }
 
+        int argc = 0;
         char *tok = strtok(inb, " ");
-        while (tok && argc < MAXARGS - 1) {
+        while (tok) {
             argv[argc++] = tok;
             tok = strtok(NULL, " ");
         }
         argv[argc] = NULL;
 
-        if (argc == 0)
+        if (argc == 0) {
+            free(argv);
             continue;
+        }
 
         int background = 0;
         if (strcmp(argv[argc-1], "&") == 0) {
@@ -78,50 +84,61 @@ int main(void) {
           argv[--argc] = NULL;
         }
 
-        if (strcmp(argv[0], "exit") == 0)
+        if (strcmp(argv[0], "exit") == 0) {
+            free(argv);
             break;
+        }
 
         if (strcmp(argv[0], "cd") == 0) {
             builtin_cd(argc, argv);
+            free(argv);
             continue;
         }
 
         if (strcmp(argv[0], "pwd") == 0) {
             builtin_pwd();
+            free(argv);
             continue;
         }
 
         if (strcmp(argv[0], "ls") == 0) {
             builtin_ls(argc, argv);
+            free(argv);
             continue;
         }
 
         if (strcmp(argv[0], "echo") == 0) {
             builtin_echo(argc, argv);
+            free(argv);
             continue;
         }
 
         if (strcmp(argv[0], "clear") == 0) {
             builtin_clear();
+            free(argv);
             continue;
         }
 
         if (strcmp(argv[0], "cat") == 0) {
             builtin_cat(argc, argv);
+            free(argv);
             continue;
         }
 
         if (strcmp(argv[0], "whoami") == 0) {
             builtin_whoami();
+            free(argv);
             continue;
         }
 
         if (strcmp(argv[0], "jobs") == 0) {
             builtin_jobs(job_pids, &job_count);
+            free(argv);
             continue;
         }
         if (strcmp(argv[0], "fg") == 0) {
             builtin_fg(argc, argv);
+            free(argv);
             continue;
         }
 
@@ -131,6 +148,7 @@ int main(void) {
             if (execvp(argv[0], argv) == -1) {
                 fprintf(stderr, "sh: %s: command not found\n", argv[0]);
             }
+            free(argv);
             exit(EXIT_FAILURE);
         } else if (pid < 0) {
             perror("fork");
@@ -147,6 +165,7 @@ int main(void) {
                 waitpid(pid, &status, WUNTRACED);
             }
         }
+        free(argv);
     }
     return 0;
 }
